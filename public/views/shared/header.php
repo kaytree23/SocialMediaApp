27fd5645ea9155322c0112631157357635e2340a
kaytree23 <katy.pacheco@ucdenver.edu>
2025-04-29 22:25:51 -0600

    header

--- public/views/shared/header.php
+++ public/views/shared/header.php
@@ -5,7 +5,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?= isset($title) ? htmlspecialchars($title) : 'Connect' ?></title>
-    <link rel="stylesheet" href="/assets/styles.css">
+    <link rel="stylesheet" href="/SocialMediaApp/public/assets/styles.css">
+    
 </head>
 <body>
 <header>
